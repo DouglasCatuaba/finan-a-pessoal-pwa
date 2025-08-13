8bba89c9fb870609063fcaf1c70874b1b381024d
Douglas Mota Catuaba <95254417+DouglasCatuaba@users.noreply.github.com>
2025-08-13 15:24:06 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,1 +1,9 @@
-{"status":500,"name":"Error","message":"Input buffer contains unsupported image format"}
+// vite.config.ts ou .js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
+
+export default defineConfig({
+  base: '/finan-a-pessoal-pwa/', // 
+  plugins: [react(), VitePWA({ registerType: 'autoUpdate' })],
+})
